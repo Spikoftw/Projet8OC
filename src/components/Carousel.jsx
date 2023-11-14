469640ffbef4ccabb0e34d0c4ff4a17fe0602d71
Spikoftw <cyril.cantonnet@gmail.com>
2023-11-14 14:17:09 +0100

    Logique Fleche carousel disparait quand 1 seule image

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -19,36 +19,39 @@ const Carousel = ({ pictures }) => {
     setCurrent(current < picturesAmount - 1 ? current + 1 : 0);
   };
 
+  const showButtons = picturesAmount > 1;
+
   return (
     <div className="carousel">
       <div className="carousel__content">
         <CarouselImg src={pictures[current]} />
       </div>
 
-      <div className="carousel__buttons">
-        <button
-          onClick={prevPicture}
-          className="carousel__button"
-          type="button"
-        >
-          <FontAwesomeIcon
-            className="carousel__arrow carousel__arrow--left"
-            icon={faChevronLeft}
-          />
-        </button>
+      {showButtons && (
+        <div className="carousel__buttons">
+          <button
+            onClick={prevPicture}
+            className="carousel__button"
+            type="button"
+          >
+            <FontAwesomeIcon
+              className="carousel__arrow carousel__arrow--left"
+              icon={faChevronLeft}
+            />
+          </button>
 
-        <button
-          onClick={nextPicture}
-          className="carousel__button"
-          type="button"
-        >
-          <FontAwesomeIcon
-            className="carousel__arrow carousel__arrow--right"
-            icon={faChevronRight}
-          />
-        </button>
-      </div>
-
+          <button
+            onClick={nextPicture}
+            className="carousel__button"
+            type="button"
+          >
+            <FontAwesomeIcon
+              className="carousel__arrow carousel__arrow--right"
+              icon={faChevronRight}
+            />
+          </button>
+        </div>
+      )}
       <div className="carousel__tracking">{`${
         current + 1
       }/${picturesAmount}`}</div>
